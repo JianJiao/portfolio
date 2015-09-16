1c3ee203b06b67f35b324db2a3c224333987a8b6
JianJiao <jianj@andrew.cmu.edu>
2015-09-15 21:20:03 -0700

    add: differentiate row and col for slideshow

--- client/src/config.js
+++ client/src/config.js
@@ -7,36 +7,42 @@ config.projects = [
     description: 'Online Data Analytics Workflow IDE: a NSF and NASA sponsored project aiming to build a web-based workflow editor to design, compose, execute and manage scientific workflows',
     links: ['http://einstein.sv.cmu.edu:9016/', 'http://einstein.sv.cmu.edu:9016/'],
     picLinks: ['img/vt.png', 'img/vtII.png'],
+    position: 'col',
   },
   {
     title: 'IOS Games',
     description: 'Developed IOS games using Objective-C, Cocos2D (Chipmunk Physics), and SpriteBuilder: ',
     links: ['https://github.com/JianJiao/Transform.git', 'https://github.com/JianJiao/Transform.git'],
     picLinks: ['img/transform.png', 'img/transformII.png'],
+    position: 'row',
   },
   {
     title: 'Twitter Big Data Analytics Web Service',
     description: 'Performed the ETL process of 1 TB Twitter dataset using AWS resources and mplemented RESTful APIs for different queries with throughput of over 10000 rps',
     links: ['https://github.com/JianJiao/Transform.git', 'https://github.com/JianJiao/Transform.git'],
     picLinks: ['img/cc.png', 'img/ccII.png'],
+    position: 'col',
   },
   {
     title: 'Survivable Social Network',
     description: 'a social network system supporting features like information sharing, group chat, searching etc',
     links: ['#', '#'],
     picLinks: ['img/fse.png', 'img/fseII.png'],
+    position: 'col',
   },
   {
     title: 'Parallel Programming Optimization',
     description: 'Implemented the parallel version of k-means clustering and Evolutionary Algorithm using CUDA and OpenMP',
     links: ['#', '#'],
     picLinks: ['img/hwfc.png', 'img/hwfcII.png'],
+    position: 'col',
   },
   {
     title: 'Workflow & Web Service Analytics and Visualization',
     description: 'Analyzed data collected from Taverna and BioCatalogue based on their usage history and visualized the results using Java Universal Network/Graph Framework(JUNG)',
     links: ['https://github.com/cmusv-sc/bigdata-workflow-recommendation-ibm.git', 'https://github.com/cmusv-sc/bigdata-workflow-recommendation-ibm.git'],
     picLinks: ['img/soc.png', 'img/socII.png'],
+    position: 'col',
   },
 
 ];
